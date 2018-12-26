28f168ee4b5aa8e37683e807cde961c307b260d9
yngtodd <young.todd.mk@gmail.com>
2018-12-26 01:22:44 -0500

    update

---
+++ everyday/calendar/settings.py
@@ -0,0 +1,17 @@
+class Settings:
+
+    def __init__(self):
+        self.justification = {
+            0: 'center',
+            1: 'center',
+            2: 'center',
+            3: 'center',
+            4: 'center',
+            5: 'center',
+            6: 'center',
+            7: 'center',
+            8: 'center',
+            9: 'center',
+            10: 'center',
+            11: 'center',
+        }
